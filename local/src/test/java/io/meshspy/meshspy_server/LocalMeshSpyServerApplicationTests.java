888d4f21ece946b3669fe8fb02e203f2378c2dde
Salvatore D'Amico <s.damico@acelli.it>
2025-05-23 12:11:00 +0200

    Second Step

---
+++ local/src/test/java/io/meshspy/meshspy_server/LocalMeshSpyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meshspy.meshspy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMeshSpyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
