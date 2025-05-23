a4da2c5155d2faa8019bf6024d5d83a31a673a4c
Salvatore D'Amico <s.damico@acelli.it>
2025-05-23 11:38:09 +0200

    First try

---
+++ src/main/java/io/meshspy/meshspy_server/MeshSpyServerApplication.java
@@ -0,0 +1,13 @@
+package io.meshspy.meshspy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshSpyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeshSpyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/meshspy/meshspy_server/MeshSpyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meshspy.meshspy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeshSpyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
