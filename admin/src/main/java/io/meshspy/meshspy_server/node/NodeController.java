c4550652e536dbf9b71de803dbdf4d1b7d34d06c
Niccolò Malenotti <malenottinicco@gmail.com>
2025-07-09 20:19:52 +0200

    Fix docker paths and clarify admin startup

--- admin/src/main/java/io/meshspy/meshspy_server/node/NodeController.java
+++ admin/src/main/java/io/meshspy/meshspy_server/node/NodeController.java
@@ -1,5 +1,7 @@
 package io.meshspy.meshspy_server.node;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/nodes")
 public class NodeController {
+    private static final Logger log = LoggerFactory.getLogger(NodeController.class);
     private final NodeService nodeService;
 
     public NodeController(NodeService nodeService) {
@@ -16,17 +19,20 @@ public class NodeController {
 
     @GetMapping
     public List<Node> listNodes() {
+        log.debug("API GET /nodes");
         return nodeService.listNodes();
     }
 
     @GetMapping("/{id}")
     public Node getNode(@PathVariable String id) {
+        log.debug("API GET /nodes/{}", id);
         return nodeService.getNode(id).orElseThrow(() -> new NodeNotFoundException(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Node addNode(@RequestBody Node node) {
+        log.debug("API POST /nodes {}", node.getId());
         return nodeService.addNode(node);
     }
 }
